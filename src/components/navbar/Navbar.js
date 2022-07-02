202c96b9e2fd1f2b2c150e96b6d3c8b8b3e59543
Melvin-Jovano <melvinjovano2@gmail.com>
2022-07-02 08:44:14 +0700

    add changes on navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -6,9 +6,12 @@ import groupSVG from './group.svg';
 import penSVG from './pen.svg';
 import globeSVG from './globe.svg';
 import homeSVG from './home.svg';
+import arrowBottomSVG from './arrow_bottom.svg';
 import searchSVG from './search.svg';
+import pencilSVG from './pencil.svg';
 import {logoSVG} from './logoSVG';
 import {noteSVG} from './noteSVG';
+import {FaPlay} from 'react-icons/fa'
 
 export default class Navbar extends Component {
 
@@ -22,8 +25,30 @@ export default class Navbar extends Component {
     render() {
         return (
             <Fragment>
+                <div className="modal fade" id="modalCreatePostNavbar" tabIndex="-1" role="dialog" aria-labelledby="modalCreatePostNavbarTitle" aria-hidden="true">
+                    <div className="modal-dialog modal-dialog-centered" role="document">
+                        <div className="modal-content modal-create-post-navbar">
+                        <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalLongTitle">Modal title</h5>
+                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div className="modal-body">
+                            ...
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="button" className="btn btn-primary">Save changes</button>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div id="modal-tooltip-remover-navbar" className="d-none modal-tooltip-remover-navbar position-absolute vw-100 vh-100"></div>
+
                 <nav className="navbar-component">
-                    <div className="container icon-navbar-color d-flex justify-content-center">
+                    <div className="container d-flex justify-content-center">
                         <div className="mx-3">
                             <NavLink to={'/'}>
                                 {logoSVG}
@@ -62,38 +87,53 @@ export default class Navbar extends Component {
 
                         <div className="d-flex align-items-center mx-3">
                             <div className="position-relative">
-                                <div className="position-absolute search-icon">
+                                <div className="position-absolute search-icon-navbar">
                                     <img src={searchSVG} alt="" className=""/>
                                 </div>
                                 <input className="fw-600 input-navbar text-white" placeholder="Search Quora"/>
                             </div>
                         </div>
 
-                        <div className="d-flex align-items-center mx-3">
-                            <div className="fw-600 text-main-color badge-navbar px-3 py-1">
+                        <div className="d-flex align-items-center mx-2">
+                            <div className="fw-600 text-main-color-navbar badge-navbar px-3 py-1">
                                 Try Quora+
                             </div>
                         </div>
 
-                        <div className="d-flex align-items-center mx-3">
+                        <div className="d-flex align-items-center mx-2">
                             <div className="">
                                 <img src="https://qph.cf2.quoracdn.net/main-thumb-750310938-50-vqunkwslayskhlspuddbrbcefzltdjge.jpeg" width={24} height={24} className="rounded-circle" alt=""/>
                             </div>
                         </div>
 
-                        <div className="d-flex align-items-center mx-3">
+                        <div className="d-flex align-items-center mx-2">
                             <div className="">
                                 <img src={globeSVG} width={24} height={24} className="rounded-circle" alt=""/>
                             </div>
                         </div>
 
-                        <div className="d-flex align-items-center mx-3">
-                            <div className="">
-                                <img src={globeSVG} width={24} height={24} className="rounded-circle" alt=""/>
+                        <div className="d-flex align-items-center mx-2">
+                            <div className="btn d-flex align-items-center position-relative button-tooltip-navbar text-light fw-600 py-1 pl-3 border-0">
+                                Add Question
+                                &emsp;
+                                <div id="tooltip-navbar">
+                                    <img src={arrowBottomSVG} alt=""/>
+                                
+                                    <div id="modal-tooltip-navbar" data-toggle="modal" data-target="#modalCreatePostNavbar" className="d-none align-items-center position-absolute tooltip-badge-navbar px-2 py-1">
+                                        <div className="position-absolute pointer-tooltip-navbar">
+                                            <FaPlay />
+                                        </div>
+                                        <img src={pencilSVG} alt="" width={18} height={18}/>
+                                        &nbsp;
+                                        Create post
+                                        &nbsp;
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </nav>
+                
             </Fragment>
         );
     }
