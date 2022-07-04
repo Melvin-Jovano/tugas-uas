92f643cee8ab98051a0f878d445f1df36ec119f2
mhduusop <mhduusop@gmail.com>
2022-07-05 04:59:14 +0700

    add content at following page

--- src/following/Following.js
+++ src/following/Following.js
@@ -17,20 +17,27 @@ class Following extends React.Component{
     
     render() {
         const followingData = this.state.data.map((data) => {
-            return (
-                <div className="row"> 
-                    <div className="col-lg-12">
-                        <div className="content-wrap">
-                            <img  className="img-content" alt="" src={data.img} width={40} height={40} />
-                            <div className="text-body">
-                                <span className="title">{data.title}</span>
-                                <span className="subtitle">{data.subtitle}</span>
-                                <span className="text">{data.text}</span>
-                            </div>
-                        </div>    
+            data.map((item)=>{
+                return(
+                    <div className="content-wrap">
+                        <img  className="img-content" alt="" src={item.img} width={40} height={40} />
+                        <div className="text-body">
+                            <span className="title">{item.title}</span>
+                            <span className="subtitle">{item.subtitle}</span>
+                            <span className="text">{item.text}</span>
+                        </div>
                     </div>
-                </div>
-                
+                )
+            });
+            return (
+                <div className="content-wrap">
+                    <img  className="img-content" alt="" src={data.img} width={40} height={40} />
+                    <div className="text-body">
+                        <span className="title">{data.title}</span>
+                        <span className="subtitle">{data.subtitle}</span>
+                        <span className="text">{data.text}</span>
+                    </div>
+                </div>    
             );
         }); 
         
@@ -48,10 +55,22 @@ class Following extends React.Component{
                                 <span className="subtext-build">Build your new following feed</span>
                             </div>
                             <h5 className="title mb-3">Discover Spaces</h5>
+
+                            {/* Spaces you might like */}
                             <div className="row" style={{ background : "#262626", height : "40px"}}>
                                 <h6 className="text-light mt-2">Spaces you might like</h6>
                             </div>
                             {followingData}
+
+                            {/* Technology */}
+                            <div className="row" style={{ background : "#262626", height : "40px"}}>
+                                <h6 className="text-light mt-2">Technology</h6>
+                            </div>
+                            {followingData}
+                        </div>
+
+                        <div className="todo-container">
+                            {/* blank */}
                         </div>
                     </div>
                 </div>
